abf36ded5aa6c825d856547a7618dc62f0d2cd18
Dilip Vishwakarma <devdilip87@gmail.com>
2026-01-21 15:45:09 +0000

    env variables updated

---
+++ src/vite-env.d.ts
@@ -0,0 +1,11 @@
+ // Code generated via "Slingshot" 
+/// <reference types="vite/client" />
+
+// Optional: strongly type the env vars you use
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
